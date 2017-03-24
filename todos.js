b6db4b48aa236c61779233917eca7ab33b11dd85
Jonathan Doron <yonidn7@gmail.com>
2017-03-24 23:06:56 +0300

    * Added todo.js, added task

--- todos.js
+++ todos.js
@@ -1,7 +1,9 @@
+// *** Current ***
 
 
-
+// *** Backlog ***
 // TODO - all this enter to function - handleEnemyKill();
 // TODO - spawn ++ enemy when enemy is killed and let the body stay. this way increasing the difficulty :)
+// TODO - improve the bounce function to handle a fast click(mousedown, mouseup) and a continous and longer click --> higher jump
 // DONE - player bounce up when killing the enemy, re-useable func
 // DONE - put all controls in activateControls(sprite) function
